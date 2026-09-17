d6bb4b70551f1debc21963b866a1339c0798e52b
rocahx <00110924@uca.edu.sv>
2026-09-16 22:40:55 -0600

    feat:implementa listar incidencias, buscar por id y cambiar estado

--- controllers/incidenciasController.js
+++ controllers/incidenciasController.js
@@ -1,4 +1,4 @@
-const { PRIORIDADES_VALIDAS, ESTADOS_VALIDOS } = require('../utils/helpers');
+ const { PRIORIDADES_VALIDAS, ESTADOS_VALIDOS } = require('../utils/helpers');
 
 
 const incidencias = [];
@@ -41,16 +41,56 @@ function registrarIncidencia(req, res) {
 
 // ===================== PERSONA 2 =====================
 function listarIncidencias(req, res) {
-
+  return res.status(200).json(incidencias);
 }
 
 function buscarIncidenciaPorId(req, res) {
-  
+  const id = parseInt(req.params.id);
+  const incidencia = incidencias.find((i) => i.id === id);
 
+  if (!incidencia) {
+    return res.status(404).json({ mensaje: 'Incidencia no encontrada' });
+  }
+
+  return res.status(200).json(incidencia);
 }
 
 function cambiarEstado(req, res) {
+  const id = parseInt(req.params.id);
+  const { estado } = req.body;
 
+  const incidencia = incidencias.find((i) => i.id === id);
+
+  if (!incidencia) {
+    return res.status(404).json({ mensaje: 'Incidencia no encontrada' });
+  }
+
+  if (!ESTADOS_VALIDOS.includes(estado)) {
+    return res.status(400).json({ mensaje: 'El estado es obligatorio' });
+  }
+
+  switch (estado) {
+    case 'Pendiente':
+      incidencia.estado = 'Pendiente';
+      break;
+
+    case 'En Proceso':
+      incidencia.estado = 'En Proceso';
+      break;
+
+    case 'Resuelta':
+      incidencia.estado = 'Resuelta';
+      break;
+
+    case 'Cancelada':
+      incidencia.estado = 'Cancelada';
+      break;
+
+    default:
+      return res.status(400).json({ mensaje: 'Estado inválido. Debe ser Pendiente, En Proceso, Resuelta o Cancelada' });
+  }
+
+  return res.status(200).json({ mensaje: 'Estado de la incidencia actualizado correctamente' , incidencia });
 }
 
 // ===================== PERSONA 3 =====================
